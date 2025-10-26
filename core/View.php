02a73e9459e689c9ccfda7e97957ac782b703437
max <www.max.021persia@gmail.com>
2025-10-26 20:39:01 +0330

    view added

---
+++ core/View.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Core;
+
+use RuntimeException;
+
+class View
+{       
+    public function  render (string $template , array $data = [] , string $layout = null):string {
+
+        $content = static::renderTemplate(
+            $template , 
+            $data 
+        ) ; 
+
+        return static::renderLayout($layout , $data , $content) ; 
+
+    }
+
+
+    protected static function renderTemplate(string $template, array $data): string
+    {
+
+        extract($data);
+
+        $path = "../app/Views/$template.php";
+
+        if (!file_exists($path)) {
+            throw new RuntimeException("Error: Template file not found: $path");
+        }
+
+        ob_start();
+        require $path;
+        return ob_get_clean();
+    }
+
+    protected static function renderLayout (?string $template , array $data , string $content):string {
+
+        if($template === null) {
+            return $content ; 
+        }
+
+        extract([...$data , 'content' => $content]) ; 
+
+        $path = "../app/Views/$template.php" ; 
+
+        if(!file_exists($path)) {
+            throw new RuntimeException("Error : Layout file not found: $path") ; 
+        }
+
+        ob_start() ; 
+        require $path ; 
+        return ob_get_clean() ; 
+
+    }
+}
